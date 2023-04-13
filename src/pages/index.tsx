a73ea9a833ce84fabda8362e7e179263d97db369
IsaqueMansur <isaquemansur@gmail.com>
2023-04-13 12:00:25 -0300

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <h1>Hello World !</h1>;
+import { PostData } from '@/domain/posts/posts';
+import axios from 'axios';
+import { GetStaticProps } from 'next';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const { data } = await axios.get(
+    'http://127.0.0.1:1337/api/posts?populate=*',
+  );
+  return data.data;
+};
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  console.log(posts);
+  return (
+    <div>
+      {posts.map((post) => (
+        <h3 key={post.id}>{post.attributes.title}</h3>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+  return {
+    props: { posts },
+  };
+};
