66469b32cf99288dae7e69f2cafc0191b7bead5b
IsaqueMansur <isaquemansur@gmail.com>
2023-04-27 15:08:11 -0300

    add Head with title and meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,19 @@ import { Container } from '@/components/MainContainer/styled';
 import { GlobalStyles } from '@/styles/global-styles';
 import { theme } from '@/styles/theme';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Antediguemon</title>
+        <meta
+          name="description"
+          content="aysalsasaaysalsasaaysalsasaaysalsasa"
+        ></meta>
+      </Head>
       <Header />
       <Container>
         <Component {...pageProps} />
