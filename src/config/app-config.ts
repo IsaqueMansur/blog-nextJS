2f22d5e08640ba2bad2addfb82372a66af9e35ef
IsaqueMansur <isaquemansur@gmail.com>
2023-04-27 11:45:40 -0300

    adjustments to using real server, dont localhost

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://127.0.0.1:1337/api';
+export const API_URL = 'http://35.198.58.243/api';
 export const ALL_POSTS_URL = `${API_URL}/posts?populate=*`;
 export const SITE_NAME = 'ARTVAC_NEXT-JS_newProject';
 export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
